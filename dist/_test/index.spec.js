29ba7c6eb9b28a9612d44baa6865e5cccd40a253
Tatyana Zherebsova <zherebtsova.ti@gmail.com>
2021-05-09 19:13:35 +0300

    add new tests

--- dist/_test/index.spec.js
+++ dist/_test/index.spec.js
@@ -26,4 +26,59 @@ describe('Select', () => {
       expect(cmp.find('.v-select-result').classes()).toContain('-open');
       expect(cmp.find('.v-select-dropdown').classes()).toContain('-show');
    });
+
+   it('Change options property', () => {
+      const arr = [];
+      for (let i = 0; i < 100; i++) {
+         arr.push({
+            id: i,
+            name: `Test ${i}`
+         });
+      }
+      const cmp = createCmp({ options: arr });
+      expect(cmp.findAll('.v-select-dropdown li')).toHaveLength(100);
+   });
+
+   it('Change name-key property', async () => {
+      const arr = [];
+      for (let i = 0; i < 10; i++) {
+         arr.push({
+            id: i,
+            title: `Test ${i}`
+         });
+      }
+      const cmp = createCmp({
+         options: arr,
+         nameKey: 'title',
+         value: 2
+      });
+      await localVue.nextTick();
+      expect(cmp.find('.v-select-result-text__text').text()).toEqual('Test 2');
+   });
+
+   it('Change id-key property', async () => {
+      const arr = [];
+      for (let i = 0; i < 10; i++) {
+         arr.push({
+            uniqId: i,
+            name: `Test ${i}`
+         });
+      }
+      const cmp = createCmp({
+         options: arr,
+         idKey: 'uniqId',
+         value: 2
+      });
+      await localVue.nextTick();
+      expect(cmp.find('.v-select-result-text__text').text()).toEqual('Test 2');
+   });
+
+   it('Change placeholder property', async () => {
+      const cmp = createCmp({
+         options: [],
+         placeholder: 'Select item...'
+      });
+      await localVue.nextTick();
+      expect(cmp.find('.v-select-result-text__empty').text()).toEqual('Select item...');
+   });
 });
